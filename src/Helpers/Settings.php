aca56d3eb365c245298fcab20a9a462e238c0c20
Bulent Akgul <bulent.akgul@me.com>
2022-04-14 18:27:26 +0300

    settings prepared for working with essentials

--- src/Helpers/Settings.php
+++ src/Helpers/Settings.php
@@ -4,114 +4,125 @@ namespace Bakgul\Kernel\Helpers;
 
 class Settings
 {
-    public static function get(string $keys = '', ?callable $callback = null)
+    public static function get(string $key = '', string $keys = '', ?callable $callback = null)
     {
-        $data = config('packagify' . Text::append($keys, '.'));
+        $data = self::raw($key, $keys);
 
         return $callback && $data ? array_filter($data, $callback) : $data;
     }
 
+    public static function raw(string $key, string $keys = '')
+    {
+        return config('packagify.essentials' . Text::append($keys, '.'))
+            ?? config('packagify' . Text::append($key, '.') . Text::append($keys, '.'));
+    }
+
     public static function default(string $navigate, string $keys = '')
     {
-        $data = self::get($navigate . Text::append($keys, '.')) ?? [''];
+        $data = self::get($navigate, $keys) ?? [''];
 
         return is_array($data) ? array_values($data)[0] : $data;
     }
 
     public static function apps(string $keys = '', ?callable $callback = null)
     {
-        return self::get('apps' . Text::append($keys, '.'), $callback);
+        return self::get('apps', $keys, $callback);
     }
 
     public static function code(string $keys = '', ?callable $callback = null)
     {
-        return self::get('code' . Text::append($keys, '.'), $callback);
+        return self::get('code', $keys, $callback);
     }
 
     public static function evaluator(string $keys = '', ?callable $callback = null)
     {
-        return self::get('evaluator' . Text::append($keys, '.'), $callback);
+        return self::get('evaluator', $keys, $callback);
     }
 
     public static function files(string $keys = '', ?callable $callback = null)
     {
-        return self::get('files' . Text::append($keys, '.'), $callback);
+        return self::get('files', $keys, $callback);
     }
 
     public static function folders(string $keys = '', ?callable $callback = null, ?bool $nullable = false)
     {
-        $folder = self::get('folders' . Text::append($keys, '.'), $callback);
+        $folder = self::get('folders', $keys, $callback);
 
         return $folder ?: ($nullable ? null : $keys);
     }
 
     public static function identity(string $keys = '', ?callable $callback = null)
     {
-        return self::get('identity' . Text::append($keys, '.'), $callback);
+        return self::get('identity', $keys, $callback);
     }
 
     public static function main(string $keys = '', ?callable $callback = null)
     {
-        return self::get('main' . Text::append($keys, '.'), $callback);
+        return self::get('main', $keys, $callback);
     }
 
     public static function messages(string $keys = '', ?callable $callback = null)
     {
-        return self::get('messages' . Text::append($keys, '.'), $callback);
+        return self::get('messages', $keys, $callback);
+    }
+
+    public static function prefixes(string $keys = '', ?callable $callback = null)
+    {
+        return self::get('prefixes', $keys, $callback);
     }
 
     public static function npm(string $keys = '', ?callable $callback = null)
     {
-        return self::get('npm' . Text::append($keys, '.'), $callback);
+        return self::get('npm', $keys, $callback);
     }
 
     public static function requires(string $keys = '', ?callable $callback = null)
     {
-        return array_values(self::get('requires' . Text::append($keys, '.'), $callback));
+        return array_values(self::get('requires', $keys, $callback));
     }
 
     public static function resourceOptions(string $keys = '', ?callable $callback = null)
     {
-        return self::get('resource_options' . Text::append($keys, '.'), $callback);
+        return self::get('resource_options', $keys, $callback);
     }
 
     public static function resources(string $keys = '', ?callable $callback = null)
     {
-        return self::get('resources' . Text::append($keys, '.'), $callback);
+        return self::get('resources', $keys, $callback);
     }
 
     public static function roots(string $keys = '', ?callable $callback = null)
     {
-        return self::get('roots' . Text::append($keys, '.'), $callback);
+        return self::get('roots', $keys, $callback);
     }
 
     public static function prohibitives(string $keys = '', ?callable $callback = null)
     {
-        return self::get('prohibitives' . Text::append($keys, '.'), $callback);
+        return self::get('prohibitives', $keys, $callback);
     }
 
     public static function seeders(string $keys = '', ?callable $callback = null)
     {
-        return self::get('seeders' . Text::append($keys, '.'), $callback);
+        return self::get('seeders', $keys, $callback);
     }
 
     public static function seperators(string $keys = '', ?callable $callback = null)
     {
-        return self::get('seperators' . Text::append($keys, '.'), $callback);
+        return self::get('seperators', $keys, $callback);
     }
 
     public static function standalone(string $key = '')
     {
-        return $key ? self::get("main.standalone_{$key}") : self::standalone('laravel') || self::standalone('package');
+        return $key ? self::get('main', "standalone_{$key}") : self::standalone('laravel') || self::standalone('package');
     }
 
     public static function structures(string $keys = '', ?callable $callback = null)
     {
-        return self::get('structures' . Text::append($keys, '.'), $callback);
+        return self::get('structures', $keys, $callback);
     }
 
     public static function symbols(string $keys = '', ?callable $callback = null)
     {
-        return self::get('symbols' . Text::append($keys, '.'), $callback);
+        return self::get('symbols', $keys, $callback);
     }
 }
