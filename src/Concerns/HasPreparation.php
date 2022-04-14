b814029a21480e78ae96d5f0a926c6ba5f5ef9a1
Bulent Akgul <bulentakgul1980@gmail.com>
2022-04-14 11:21:01 +0300

    new method to set arguments when repo is standalone

--- src/Concerns/HasPreparation.php
+++ src/Concerns/HasPreparation.php
@@ -2,6 +2,9 @@
 
 namespace Bakgul\Kernel\Concerns;
 
+use Bakgul\Kernel\Helpers\Arry;
+use Bakgul\Kernel\Helpers\Settings;
+
 trait HasPreparation
 {
     public array $request;
@@ -10,13 +13,25 @@ trait HasPreparation
     public function prepareRequest(): void
     {
         $this->request = array_filter(
-            array_merge($this->arguments(), $this->setOptions()),
+            array_merge($this->setArguments(), $this->setOptions()),
             fn ($x) => !in_array($x, $this->uselessKeys),
             ARRAY_FILTER_USE_KEY
         );
     }
 
-    private function setOptions()
+    private function setArguments(): array
+    {
+        $arguments = $this->arguments();
+
+        if (Settings::standalone()) {
+            $arguments['app'] = Arry::get($arguments, 'package');
+            $arguments['package'] = null;
+        }
+        
+        return $arguments;
+    }
+
+    private function setOptions(): array
     {
         return array_map(fn ($x) => $this->clear($x), $this->options());
     }
