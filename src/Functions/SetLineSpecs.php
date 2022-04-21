ed42dfdc357ec2363cb036ca01be78740b209c5a
Bulent Akgul <bulent.akgul@me.com>
2022-04-21 12:28:33 +0300

    new default added

--- src/Functions/SetLineSpecs.php
+++ src/Functions/SetLineSpecs.php
@@ -7,6 +7,7 @@ class SetLineSpecs
     public static function _(array $specs): array
     {
         return array_merge([
+            'start' => ['use', 0],
             'isStrict' => false,
             'part' => '',
             'repeat' => 0,
