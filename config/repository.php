63ebb4aa2b4806173ad818380815c1e9d4f53ee9
Bulent Akgul <bulent.akgul@me.com>
2022-05-13 15:33:01 +0300

    repository comment block updated

--- config/repository.php
+++ config/repository.php
@@ -10,12 +10,13 @@ return [
     | and you will tell which type it's by changing the settings here.
     | 
     | Standalone Laravel: Meke it "true" if it's a very standard Laravel app.
-    | Standalone Package: Make it "true" if you are developing a package.
+    | Standalone Package: Make it "true" if you are developing a package that
+    |                     will be installed through composer.
     |
-    | If both standalone settings are "false", then your repository will be
-    | a packagified Laravel. This means you will create the functionalities
-    | in the packages instead of "app" or "resources" folder, but those
-    | default folders will be in use.
+    | If both standalone settings are set to "false", then your repository
+    | will be a packagified Laravel. This means you will have a normal
+    | Laravel repository, but the functionalities will be splitted into
+    | the packages instead of "app" and "resources" folders.
     |
     */
     'standalone_laravel' => false,
