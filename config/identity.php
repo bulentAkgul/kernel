c519054712910101eecf91733ba79269aa23ca4e
Bulent Akgul <bulent.akgul@me.com>
2022-05-18 10:07:36 +0300

    comment block improved

--- config/identity.php
+++ config/identity.php
@@ -8,12 +8,15 @@ return [
     |
     | Some main settings of your repository.
     | 
-    | Vendor, Name, Email, and Package* are self-explanatory.
-    | 
-    | Registrar*: It's a shorthand for registering your package files
-    |             such as blade views and config.
-    | Namespace*: The default value of any "null" namespace in "roots."
-    | *: It'll be used when the repository is a standalone package.
+    | Vendor, Name, and Email: These are the personal pieces of information 
+    |                          used in the "composer.json" files.
+    | Package: It must be unique throughout the app. It will be used only 
+    |          in the Packagified Laravel app.
+    | Registrar: It's a shorthand for registering your package files such as 
+    |            blade views and config in a standalone package. It'll collect 
+    |            the packages' config values in Packagified Laravel.
+    | Namespace: The default value of any "null" namespace in "roots" and the 
+    |            namespace of the standalone package. 
     |
     */
     'vendor' => 'bakgul',
