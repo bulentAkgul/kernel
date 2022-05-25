7fb1a3ea4c96fcb0750ea5ee0d2360b59349f664
Bulent Akgul <bulentakgul1980@gmail.com>
2022-05-25 10:03:57 +0300

    resource file simulator added

--- src/Tasks/SimulateArtisanCall.php
+++ src/Tasks/SimulateArtisanCall.php
@@ -5,9 +5,12 @@ namespace Bakgul\Kernel\Tasks;
 use Bakgul\FileCreator\Commands\CreateFileCommand;
 use Bakgul\FileCreator\Services\FileService;
 use Bakgul\Kernel\Concerns\HasRequest;
+use Bakgul\Kernel\Helpers\Arry;
 use Bakgul\Kernel\Tasks\MakeFileList;
 use Bakgul\PackageGenerator\Commands\CreatePackageCommand;
 use Bakgul\PackageGenerator\Services\PackageService;
+use Bakgul\ResourceCreator\Commands\CreateResourceCommand;
+use Bakgul\ResourceCreator\Services\ResourceService;
 
 class SimulateArtisanCall
 {
@@ -19,12 +22,18 @@ class SimulateArtisanCall
     public function __invoke($request, $command = 'file')
     {
         $this->request = $request;
-        
+
         match ($command) {
-            'file' => self::simulateFileCommand(MakeFileList::_($request)),
+            'file' => self::simulateFileCommand($this->queue($request)),
+            'resource' => self::simulateResourceCommand($this->queue($request)),
             'package' => self::simulatePackageCommand(),
             default => null
         };
+    }
+
+    private function queue($request)
+    {
+        return Arry::get($request, 'queue') ?? MakeFileList::_($request);
     }
 
     private function simulateFileCommand($queue)
@@ -33,6 +42,15 @@ class SimulateArtisanCall
 
         foreach ($queue as $file) {
             (new FileService)->create($this->makeFileRequest($file, $queue));
+        }
+    }
+
+    private function simulateResourceCommand($queue)
+    {
+        $this->command = new CreateResourceCommand;
+
+        foreach ($queue as $file) {
+            (new ResourceService)->create($this->makeFileRequest($file, $queue));
         }
     }
 
