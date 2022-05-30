b189c7b78f58a1254bcab31afc804560be4629ae
Bulent Akgul <bulent.akgul@me.com>
2022-05-30 19:09:11 +0300

    no dump while testing

--- src/Functions/DumpFeedback.php
+++ src/Functions/DumpFeedback.php
@@ -3,12 +3,14 @@
 namespace Bakgul\Kernel\Functions;
 
 use Bakgul\Kernel\Helpers\Text;
-use Bakgul\Kernel\Tasks\GetClass;
+use Illuminate\Support\Facades\App;
 
 class DumpFeedback
 {
     public static function _(string $path, string $type, string $glue = DIRECTORY_SEPARATOR): void
     {
+        if (App::runningUnitTests()) return;
+        
         $name = Text::getTail($path, $glue);
         $path = Text::dropTail(trim(str_replace(base_path(), '', $path), $glue), $glue);
 
